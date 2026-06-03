842d66311f2f106348e1219b7f53a9d786f355b2
Negar <negarhasani1379@gmail.com>
2026-06-03 16:43:45 +0330

    create views

--- books/views.py
+++ books/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def home(request):
+    return render(request,'books/home.html')
+
+
+def book_list(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request,'books/book_list.html', context)
+
+def book_detail(request,id):
+    book = Book.objects.get(id=id)
+    context = {'book': book}
+    return render(request,'books/book_detail.html', context)
+
+def book_create_update(request):
+    return render(request)
+
